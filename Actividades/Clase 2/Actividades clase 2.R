37aee703e0258fd8ce2c2a388267f380d1640da0
Ruben Soza <rsozac@uc.cl>
2019-10-17 17:39:23 -0300

    Clase 2 Final

--- Actividades/Clase 2/Actividades clase 2.R
+++ Actividades/Clase 2/Actividades clase 2.R
@@ -3,8 +3,9 @@
 # Actividad 1 #
 library(readxl)
 library(tidyverse)
-Encuesta <- read_excel('Datasets/encuesta.xlsx')
-Encuesta %>% 
+encuesta <- read_excel('Datasets/encuesta.xlsx')
+?select_if
+encuesta %>% 
   select(P3,P8,P9,P154,P156) %>%
   filter(P9 == "Hombre", P3 == "Región de Valparaíso") %>%
   arrange(P8) %>%
@@ -12,3 +13,9 @@ Encuesta %>%
   group_by(P154) %>%
   summarise(mean(PRSC))
 
+
+encuesta %>% 
+  select_if(str_detect(encuesta,"Región"))
+
+is_whole <- function(x) all(floor(x) == x)
+select_if(mtcars, is_whole)
